6dbb8c945d97e3c4ddaffb84ef1495ff56f6c4ee
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-03 14:05:23 -0700

    added more constants

--- src/main/java/com/epam/esm/util/Constants.java
+++ src/main/java/com/epam/esm/util/Constants.java
@@ -3,4 +3,8 @@ package com.epam.esm.util;
 public final class Constants {
     public static final int MAX_GIFT_CERTIFICATE_NAME_LENGTH = 64;
     public static final int MAX_GIFT_CERTIFICATE_DESCRIPTION_LENGTH = 255;
+    public static final int MAX_TAG_NAME_LENGTH = 64;
+    public static final String DEFAULT_PAGE = "0";
+    public static final String DEFAULT_PAGE_SIZE = "10";
+    public static final String DEFAULT_SORT = "id,asc";
 }
