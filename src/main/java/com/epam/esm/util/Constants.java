2c441a910d0b3d2ead667b491b5699e4562c9c91
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-02 23:09:36 -0700

    added constants with db constrains

---
+++ src/main/java/com/epam/esm/util/Constants.java
@@ -0,0 +1,6 @@
+package com.epam.esm.util;
+
+public final class Constants {
+    public static final int MAX_GIFT_CERTIFICATE_NAME_LENGTH = 64;
+    public static final int MAX_GIFT_CERTIFICATE_DESCRIPTION_LENGTH = 255;
+}
