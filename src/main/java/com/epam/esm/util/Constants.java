841f91666e128d277461231c4304df8971759420
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-04 10:03:37 -0700

    - added allowed sort fileds and allowed sort directions to Constants

--- src/main/java/com/epam/esm/util/Constants.java
+++ src/main/java/com/epam/esm/util/Constants.java
@@ -6,5 +6,7 @@ public final class Constants {
     public static final int MAX_TAG_NAME_LENGTH = 64;
     public static final String DEFAULT_PAGE = "0";
     public static final String DEFAULT_PAGE_SIZE = "10";
-    public static final String DEFAULT_SORT = "id,asc";
+    public static final String DEFAULT_SORT = "name,asc";
+    public static final String[] ALLOWED_SORT_FIELDS = {"name", "create_date","update_date"};
+    public static final String[] ALLOWED_SORT_DIRECTIONS = {"asc", "desc"};
 }
