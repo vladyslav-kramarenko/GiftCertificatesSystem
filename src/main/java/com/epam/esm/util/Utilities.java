f379709a6008133c5fae23cdd655bbd1ad79a0a9
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-04 13:22:03 -0700

    - added Javadoc - refactoring

--- src/main/java/com/epam/esm/util/Utilities.java
+++ src/main/java/com/epam/esm/util/Utilities.java
@@ -5,15 +5,30 @@ import org.springframework.data.domain.Sort;
 import org.springframework.jdbc.support.KeyHolder;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 
+/**
+ * A utility class with helper methods.
+ */
 public class Utilities {
-    public static boolean validateId(Long id) {
-        if (id != null && id >= 0) {
-            return true;
-        }
-        throw new IllegalArgumentException("Wrong ID: " + id);
+    /**
+     * Validates if the given ID is not null and greater than or equal to zero.
+     *
+     * @param id the ID to validate
+     * @throws IllegalArgumentException if the ID is not valid
+     */
+    public static void validateId(Long id) {
+        if (id == null || id < 0)
+            throw new IllegalArgumentException("Wrong ID: " + id);
     }
 
+    /**
+     * Returns an ORDER BY clause for the given SQL query based on the provided Sort object.
+     *
+     * @param sql  the SQL query to modify
+     * @param sort the Sort object containing sort parameters
+     * @return the modified SQL query with the ORDER BY clause
+     */
     public static String getOrderByClause(String sql, Sort sort) {
         if (sort == null || sort.isEmpty()) return sql;
         StringBuilder sb = new StringBuilder(sql);
@@ -31,15 +46,26 @@ public class Utilities {
         return sb.toString();
     }
 
+    /**
+     * Returns the generated key from the given KeyHolder object.
+     *
+     * @param keyHolder the KeyHolder object containing the generated key
+     * @return the generated key as a long value
+     * @throws DbException if the generated key is not found or cannot be converted to a long value
+     */
     public static long getKey(KeyHolder keyHolder) throws DbException {
-        Object keyObject = keyHolder.getKeys().getOrDefault("GENERATED_KEY", null);
-        if (keyObject != null) {
-            BigInteger key = (BigInteger) keyObject;
-            return key.longValue();
-        } else {
-            keyObject = keyHolder.getKeys().get("id");
-            if (keyObject == null) throw new DbException("Generated key not found");
-            return (long) keyObject;
+        try {
+            Object keyObject = keyHolder.getKeys().getOrDefault("GENERATED_KEY", null);
+            if (keyObject != null) {
+                BigInteger key = (BigInteger) keyObject;
+                return key.longValue();
+            } else {
+                keyObject = keyHolder.getKeys().get("id");
+                if (keyObject == null) throw new DbException("Generated key not found");
+                return (long) keyObject;
+            }
+        } catch (Exception e) {
+            throw new DbException("Generated key cannot be converted to a long value: " + Arrays.toString(e.getStackTrace()));
         }
     }
 }
