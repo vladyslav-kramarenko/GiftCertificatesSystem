22b7cf79dcf876ece4381dc4cdd1dedfaec9d28c
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-04 16:39:14 -0700

    - added generateGiftCertificate method to testUtils - renamed generateTag method

--- src/test/java/com/epam/esm/util/testUtils.java
+++ src/test/java/com/epam/esm/util/testUtils.java
@@ -1,6 +1,11 @@
 package com.epam.esm.util;
 
+import com.epam.esm.model.GiftCertificate;
 import com.epam.esm.model.Tag;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class testUtils {
     public static String generateStringBySize(int size) {
@@ -11,10 +16,23 @@ public class testUtils {
         return text.toString();
     }
 
-    public static Tag generateTag(long id) {
+    public static Tag generateTagWithId(long id) {
         Tag tag = new Tag();
         tag.setId(id);
         tag.setName("Tag " + id);
         return tag;
+    }
+
+    public static GiftCertificate generateGiftCertificateWithoutId() {
+        GiftCertificate giftCertificate = new GiftCertificate();
+        giftCertificate.setName("Test Gift Certificate");
+        giftCertificate.setDescription("Test Description");
+        giftCertificate.setPrice(BigDecimal.valueOf(10.00));
+        giftCertificate.setDuration((30));
+        List<Tag> tags = new ArrayList<>();
+        tags.add(generateTagWithId(1L));
+        tags.add(generateTagWithId(2L));
+        giftCertificate.setTags(tags);
+        return giftCertificate;
     }
 }
