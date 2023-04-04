3fa6890fa100f0207257986d95a1f2b385840d3b
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-04 13:23:49 -0700

    - added Javadoc

--- src/main/java/com/epam/esm/util/TagUtils.java
+++ src/main/java/com/epam/esm/util/TagUtils.java
@@ -6,7 +6,17 @@ import java.util.List;
 
 import static com.epam.esm.util.Constants.MAX_TAG_NAME_LENGTH;
 
+/**
+ * This class provides utility methods for working with tags.
+ */
 public class TagUtils {
+    /**
+     * Validates a single tag. Throws an IllegalArgumentException if the tag's name is null or empty,
+     * or if the tag's name length is greater than MAX_TAG_NAME_LENGTH.
+     *
+     * @param tag the tag to validate
+     * @throws IllegalArgumentException if the tag is not valid
+     */
     public static void validateTag(Tag tag) throws IllegalArgumentException {
         if (tag.getName() == null || tag.getName().isEmpty()) {
             throw new IllegalArgumentException("Tag name cannot be empty");
@@ -16,6 +26,12 @@ public class TagUtils {
         }
     }
 
+    /**
+     * Validates a list of tags. Calls validateTag for each tag in the list.
+     *
+     * @param tags the list of tags to validate
+     * @throws IllegalArgumentException if any tag in the list is not valid
+     */
     public static void validateTags(List<Tag> tags) throws IllegalArgumentException {
         for (Tag tag : tags) {
             validateTag(tag);
