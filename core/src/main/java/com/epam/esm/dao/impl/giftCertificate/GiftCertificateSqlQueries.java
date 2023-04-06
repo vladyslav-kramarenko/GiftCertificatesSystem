168f8c26a08be75fe67d9cbb7c6a212e5babbac9
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-06 09:57:16 -0700

    - bug fix in gift certificate sql

--- core/src/main/java/com/epam/esm/dao/impl/giftCertificate/GiftCertificateSqlQueries.java
+++ core/src/main/java/com/epam/esm/dao/impl/giftCertificate/GiftCertificateSqlQueries.java
@@ -25,8 +25,9 @@ public final class GiftCertificateSqlQueries {
     public static final String DELETE_CERTIFICATE_BY_ID = "DELETE FROM gift_certificate WHERE id = ?";
     public static final String UPDATE_CERTIFICATE = "UPDATE gift_certificate SET name = ?, description = ?, price = ?, duration = ? WHERE id = ?";
     public static final String SELECT_CERTIFICATES_BY_TAG_ID =
-            "SELECT c.id, c.name, c.description, c.price, c.duration, c.create_date, c.last_update_date " +
-                    "FROM certificate c " +
-                    "INNER JOIN gift_certificate_has_tag ct ON c.id = ct.gift_certificate_id " +
-                    "WHERE ct.tag_id = ?";
+            "SELECT gc.id, gc.name, gc.description, gc.price, gc.duration, gc.create_date, gc.last_update_date, t.id as tag_id, t.name as tag_name " +
+                    "FROM gift_certificate gc " +
+                    "LEFT JOIN gift_certificate_has_tag gct ON gc.id = gct.gift_certificate_id " +
+                    "LEFT JOIN tag t ON gct.tag_id = t.id " +
+                    "WHERE gct.tag_id = ?";
 }
