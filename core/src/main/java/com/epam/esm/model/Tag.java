2b3f9c63b451036721635389f553d8b91aa94399
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-12 13:23:35 -0700

    - changed Tag model class to use Lombok

--- core/src/main/java/com/epam/esm/model/Tag.java
+++ core/src/main/java/com/epam/esm/model/Tag.java
@@ -1,11 +1,17 @@
 package com.epam.esm.model;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class Tag {
     private Long id;
     private String name;
-
 
     public Tag() {
     }
@@ -17,41 +23,5 @@ public class Tag {
 
     public Tag(String name) {
         this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Tag tag)) return false;
-        return Objects.equals(getName(), tag.getName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getName());
-    }
-
-    @Override
-    public String toString() {
-        return "Tag{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
     }
 }
