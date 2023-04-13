23bf831ee5f85477bbf99b67cc4e9c63e1c7b394
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-13 05:39:37 -0700

    - added Javadoc to GiftCertificate class

--- core/src/main/java/com/epam/esm/model/GiftCertificate.java
+++ core/src/main/java/com/epam/esm/model/GiftCertificate.java
@@ -11,6 +11,25 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * This class represents a Gift Certificate entity. It contains the following fields:
+ * <ul>
+ *     <li>id - a unique identifier for the gift certificate</li>
+ *     <li>name - the name of the gift certificate</li>
+ *     <li>description - a description of the gift certificate</li>
+ *     <li>price - the price of the gift certificate</li>
+ *     <li>duration - the duration (in days) of the gift certificate's validity</li>
+ *     <li>createDate - the creation date of the gift certificate</li>
+ *     <li>lastUpdateDate - the last updated date of the gift certificate</li>
+ *     <li>tags - a list of associated {@link Tag} objects</li>
+ * </ul>
+ * <p>
+ * The class is annotated with {@link Getter}, {@link Setter}, {@link EqualsAndHashCode},
+ * and {@link ToString} to automatically generate getters, setters, equals, hashCode, and toString methods.
+ * <p>
+ * The {@link LocalDateTime} fields are annotated with {@link JsonSerialize} to specify a custom serializer
+ * for serializing the LocalDateTime objects to JSON.
+ */
 @Getter
 @Setter
 @EqualsAndHashCode
