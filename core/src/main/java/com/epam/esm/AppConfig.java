da6edb4566f1ed7c241a62dd7b91bd28f35907d2
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-12 22:28:20 -0700

    - refactoring in AppConfig, added javadoc

--- core/src/main/java/com/epam/esm/AppConfig.java
+++ core/src/main/java/com/epam/esm/AppConfig.java
@@ -4,9 +4,10 @@ import com.epam.esm.dao.GiftCertificateDao;
 import com.epam.esm.dao.TagDao;
 import com.epam.esm.dao.impl.giftCertificate.GiftCertificateDaoImpl;
 import com.epam.esm.dao.impl.tag.TagDaoImpl;
+
 import com.zaxxer.hikari.HikariDataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -21,15 +22,23 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
+/**
+ * Application configuration class that sets up beans for the application.
+ */
 @EnableTransactionManagement(proxyTargetClass = true)
 @Configuration
 @PropertySource("classpath:application.properties")
 public class AppConfig {
     @Autowired
     private Environment env;
+
+    /**
+     * Creates a data source for the test environment using an H2 embedded database.
+     *
+     * @return the configured DataSource for the test environment
+     */
     @Bean
     @Profile("test")
-    @Qualifier("dataSource")
     public DataSource embeddedDataSource() {
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
@@ -37,9 +46,14 @@ public class AppConfig {
                 .addScript("classpath:data.sql")
                 .build();
     }
+
+    /**
+     * Creates a data source for the production environment using HikariCP.
+     *
+     * @return the configured DataSource for the production environment
+     */
     @Bean
     @Profile("!test")
-    @Qualifier("dataSource")
     public DataSource dataSource() {
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
@@ -50,21 +64,46 @@ public class AppConfig {
                 "spring.datasource.hikari.maximum-pool-size")));
         return dataSource;
     }
+
+    /**
+     * Creates a JdbcTemplate instance with the given DataSource.
+     *
+     * @param dataSource the DataSource to be used by the JdbcTemplate
+     * @return a new JdbcTemplate instance
+     */
     @Bean
-    public JdbcTemplate jdbcTemplate(@Qualifier("dataSource") DataSource dataSource) {
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
+
+    /**
+     * Creates a GiftCertificateDao instance with the given JdbcTemplate.
+     *
+     * @param jdbcTemplate the JdbcTemplate to be used by the GiftCertificateDao
+     * @return a new GiftCertificateDao instance
+     */
     @Bean
-    @Profile("test")
     public GiftCertificateDao giftCertificateDao(JdbcTemplate jdbcTemplate) {
         return new GiftCertificateDaoImpl(jdbcTemplate);
     }
+
+    /**
+     * Creates a TagDao instance with the given JdbcTemplate.
+     *
+     * @param jdbcTemplate the JdbcTemplate to be used by the TagDao
+     * @return a new TagDao instance
+     */
     @Bean
-    @Profile("test")
     public TagDao tagDao(JdbcTemplate jdbcTemplate) {
         return new TagDaoImpl(jdbcTemplate);
     }
 
+    /**
+     * Creates a PlatformTransactionManager instance with the given DataSource.
+     *
+     * @param dataSource the DataSource to be used by the PlatformTransactionManager
+     * @return a new PlatformTransactionManager instance
+     */
     @Bean
     public PlatformTransactionManager transactionManager(DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
