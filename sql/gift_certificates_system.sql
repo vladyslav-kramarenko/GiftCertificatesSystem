49fe2bea90a05ccdb282230a9fe20b3b577a5155
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-17 11:40:12 -0700

    - changed db SQL: added user and order entities

--- sql/gift_certificates_system.sql
+++ sql/gift_certificates_system.sql
@@ -2,7 +2,8 @@
 
 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
-SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE =
+        'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
 -- Schema gift_certificates_system
@@ -22,8 +23,7 @@ CREATE TABLE IF NOT EXISTS `gift_certificates_system`.`tag`
 (
     `id`   INT         NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(64) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE
+    PRIMARY KEY (`id`)
 );
 
 
@@ -56,13 +56,69 @@ CREATE TABLE IF NOT EXISTS `gift_certificates_system`.`gift_certificate_has_tag`
     CONSTRAINT `fk_gift_certificate_has_tag_gift_certificate`
         FOREIGN KEY (`gift_certificate_id`)
             REFERENCES `gift_certificates_system`.`gift_certificate` (`id`)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE,
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
     CONSTRAINT `fk_gift_certificate_has_tag_tag1`
         FOREIGN KEY (`tag_id`)
             REFERENCES `gift_certificates_system`.`tag` (`id`)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+);
+
+
+-- -----------------------------------------------------
+-- Table `gift_certificates_system`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gift_certificates_system`.`user`
+(
+    `id`         INT         NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(45) NOT NULL,
+    `last_name`  VARCHAR(45) NULL,
+    PRIMARY KEY (`id`)
+);
+
+
+-- -----------------------------------------------------
+-- Table `gift_certificates_system`.`user_order`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gift_certificates_system`.`user_order`
+(
+    `id`               INT            NOT NULL AUTO_INCREMENT,
+    `user_id`          INT            NOT NULL,
+    `sum`              DECIMAL(10, 2) NOT NULL,
+    `create_date`      DATETIME       NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_update_date` DATETIME       NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    INDEX `fk_order_user1_idx` (`user_id` ASC) VISIBLE,
+    CONSTRAINT `fk_order_user1`
+        FOREIGN KEY (`user_id`)
+            REFERENCES `gift_certificates_system`.`user` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+);
+
+
+-- -----------------------------------------------------
+-- Table `gift_certificates_system`.`order_has_gift_certificate`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gift_certificates_system`.`order_has_gift_certificate`
+(
+    `order_id`            INT NOT NULL,
+    `gift_certificate_id` INT NOT NULL,
+    `count`               INT NOT NULL DEFAULT 1,
+    PRIMARY KEY (`order_id`, `gift_certificate_id`),
+    INDEX `fk_order_has_gift_certificate_gift_certificate1_idx` (`gift_certificate_id` ASC) VISIBLE,
+    INDEX `fk_order_has_gift_certificate_order1_idx` (`order_id` ASC) VISIBLE,
+    CONSTRAINT `fk_order_has_gift_certificate_order1`
+        FOREIGN KEY (`order_id`)
+            REFERENCES `gift_certificates_system`.`user_order` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    CONSTRAINT `fk_order_has_gift_certificate_gift_certificate1`
+        FOREIGN KEY (`gift_certificate_id`)
+            REFERENCES `gift_certificates_system`.`gift_certificate` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
 );
 
 USE `gift_certificates_system`;
