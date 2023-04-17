265647ae8ce8d2cc34d3531f331e05e4021d8e77
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-17 11:47:39 -0700

    - added constants for User and Order

--- core/src/main/java/com/epam/esm/core/util/CoreConstants.java
+++ core/src/main/java/com/epam/esm/core/util/CoreConstants.java
@@ -3,8 +3,12 @@ package com.epam.esm.core.util;
 public final class CoreConstants {
     public static final int MAX_GIFT_CERTIFICATE_NAME_LENGTH = 64;
     public static final int MAX_GIFT_CERTIFICATE_DESCRIPTION_LENGTH = 255;
+    public static final int MAX_USER_FIRST_NAME_LENGTH = 45;
+    public static final int MAX_USER_LAST_NAME_LENGTH = 45;
     public static final int MAX_TAG_NAME_LENGTH = 64;
     public static final String[] ALLOWED_GIFT_CERTIFICATE_SORT_FIELDS = {"name", "create_date", "update_date"};
     public static final String[] ALLOWED_TAG_SORT_FIELDS = {"name", "id"};
+    public static final String[] ALLOWED_USER_SORT_FIELDS = {"first_name","last_name", "id"};
+    public static final String[] ALLOWED_ORDER_SORT_FIELDS = {"sum", "id"};
     public static final String[] ALLOWED_SORT_DIRECTIONS = {"asc", "desc"};
 }
