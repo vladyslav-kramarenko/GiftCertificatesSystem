505bf3d64c134c75e73c6cd182e643fb4cfb9b3e
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-23 22:13:06 -0700

    changed ErrorResponse to record

--- api/src/main/java/com/epam/esm/api/ErrorResponse.java
+++ api/src/main/java/com/epam/esm/api/ErrorResponse.java
@@ -1,19 +1,4 @@
 package com.epam.esm.api;
 
-public class ErrorResponse {
-    private final String errorMessage;
-    private final String errorCode;
-
-    public ErrorResponse(String errorMessage, String errorCode) {
-        this.errorMessage = errorMessage;
-        this.errorCode = errorCode;
-    }
-
-    public String getErrorMessage() {
-        return errorMessage;
-    }
-
-    public String getErrorCode() {
-        return errorCode;
-    }
+public record ErrorResponse(String errorMessage, String errorCode) {
 }
