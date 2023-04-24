be37ff0892a2216dc4102fc512ef09d022191e7d
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-23 22:09:11 -0700

    added more validations for orders

--- core/src/main/java/com/epam/esm/core/dto/OrderRequest.java
+++ core/src/main/java/com/epam/esm/core/dto/OrderRequest.java
@@ -1,16 +1,21 @@
 package com.epam.esm.core.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
+
+@Validated
 public record OrderRequest(
         @NotNull(message = "User ID should not be empty")
-        @Positive(message = "User ID should be positive")
+        @Min(value = 0L, message = "User ID should be positive")
         Long userId,
         @NotNull(message = "Order should have at least 1 gift certificate")
         @Size(min = 1, message = "Order should have at least 1 gift certificate")
+        @Valid
         List<GiftCertificateOrder> giftCertificates
 ) {
 }
