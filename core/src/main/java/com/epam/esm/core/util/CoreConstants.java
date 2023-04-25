e513f36dc4faaa5542801010b6332c2f9a6fdf70
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-04-24 23:09:15 -0700

    - added id to allowed giftCertificates sort params

--- core/src/main/java/com/epam/esm/core/util/CoreConstants.java
+++ core/src/main/java/com/epam/esm/core/util/CoreConstants.java
@@ -6,7 +6,7 @@ public final class CoreConstants {
     public static final int MAX_USER_FIRST_NAME_LENGTH = 45;
     public static final int MAX_USER_LAST_NAME_LENGTH = 45;
     public static final int MAX_TAG_NAME_LENGTH = 64;
-    public static final String[] ALLOWED_GIFT_CERTIFICATE_SORT_FIELDS = {"name", "create_date", "update_date"};
+    public static final String[] ALLOWED_GIFT_CERTIFICATE_SORT_FIELDS = {"id","name", "create_date", "update_date"};
     public static final String[] ALLOWED_TAG_SORT_FIELDS = {"name", "id"};
     public static final String[] ALLOWED_USER_SORT_FIELDS = {"first_name","last_name", "id"};
     public static final String[] ALLOWED_ORDER_SORT_FIELDS = {"sum", "id"};
