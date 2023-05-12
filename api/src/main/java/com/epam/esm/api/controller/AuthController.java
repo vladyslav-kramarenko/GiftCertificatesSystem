e3a3cc4951292fbd1d2d41054c60dea0c18eb2e1
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-05-11 23:04:00 -0700

    - added validation for register and login endpoints

--- api/src/main/java/com/epam/esm/api/controller/AuthController.java
+++ api/src/main/java/com/epam/esm/api/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.epam.esm.api.controller;
 
 import com.epam.esm.core.service.AuthService;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,17 +22,20 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestParam String email, @RequestParam String password) throws Exception {
+    public ResponseEntity<String> login(
+            @RequestParam @NotNull @Email String email,
+            @RequestParam @NotNull String password
+    ) throws Exception {
         return authService.authenticateUser(email, password);
     }
 
     @PostMapping("/register")
     @Transactional
     public ResponseEntity<?> register(
-            @RequestParam("email") String email,
-            @RequestParam("password") String password,
-            @RequestParam("firstName") String firstName,
-            @RequestParam("lastName") String lastName
+            @RequestParam(name = "email") @Email @NotNull String email,
+            @RequestParam(name = "password") @NotNull String password,
+            @RequestParam(name = "firstName", required = false) String firstName,
+            @RequestParam(name = "lastName", required = false) String lastName
     ) throws Exception {
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.registerUser(email, password, firstName, lastName));
     }
