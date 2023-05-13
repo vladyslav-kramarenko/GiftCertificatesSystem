8e2638ab47ec27c784844dc6a9f25e518ec56993
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-05-12 19:49:21 -0700

    - added constants with roles and times

--- core/src/main/java/com/epam/esm/core/util/CoreConstants.java
+++ core/src/main/java/com/epam/esm/core/util/CoreConstants.java
@@ -6,9 +6,17 @@ public final class CoreConstants {
     public static final int MAX_USER_FIRST_NAME_LENGTH = 45;
     public static final int MAX_USER_LAST_NAME_LENGTH = 45;
     public static final int MAX_TAG_NAME_LENGTH = 64;
-    public static final String[] ALLOWED_GIFT_CERTIFICATE_SORT_FIELDS = {"id","name", "create_date", "update_date"};
+
+    public static final int ONE_MINUTE = 60 * 1000;
+    public static final int ONE_HOUR = 60 * 60 * 1000;
+
+    public static final int ONE_DAY = 24 * 60 * 60 * 1000;
+    public static final String[] ALLOWED_GIFT_CERTIFICATE_SORT_FIELDS = {"id", "name", "create_date", "update_date"};
     public static final String[] ALLOWED_TAG_SORT_FIELDS = {"name", "id"};
-    public static final String[] ALLOWED_USER_SORT_FIELDS = {"first_name","last_name", "id"};
+    public static final String[] ALLOWED_USER_SORT_FIELDS = {"first_name", "last_name", "id"};
     public static final String[] ALLOWED_ORDER_SORT_FIELDS = {"sum", "id"};
     public static final String[] ALLOWED_SORT_DIRECTIONS = {"asc", "desc"};
+    public static final String ROLE_MANAGER = "MANAGER";
+    public static final String ROLE_USER = "USER";
+    public static final String ROLE_ADMIN = "ADMIN";
 }
