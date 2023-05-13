e325fcfc917ca69e338caa11c8df4e6430dca0c3
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-05-12 19:49:56 -0700

    - created Custom User Details with userId

---
+++ core/src/main/java/com/epam/esm/core/dto/CustomUserDetails.java
@@ -0,0 +1,19 @@
+package com.epam.esm.core.dto;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class CustomUserDetails extends User {
+    private Long id;
+
+    public CustomUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities, Long id) {
+        super(username, password, authorities);
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
