e0646c6d76a6123e88608ad1ed72c59279d59ac5
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-05-19 19:47:31 -0700

    - refactoring

--- core/src/main/java/com/epam/esm/core/service/impl/auth/auth0/Auth0JwtAuthConverterService.java
+++ core/src/main/java/com/epam/esm/core/service/impl/auth/auth0/Auth0JwtAuthConverterService.java
@@ -13,38 +13,50 @@ import org.springframework.stereotype.Service;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
+
+import static com.epam.esm.core.util.CoreConstants.AUTH0_EMAIL_CLAIM;
+
 @Service
-public class Auth0JwtTokenService {
+public class Auth0JwtAuthConverterService {
+
     private final UserService userService;
-
+    private final JwtGrantedAuthoritiesConverter authoritiesConverter;
     @Autowired
-    public Auth0JwtTokenService(UserService userService) {
+    public Auth0JwtAuthConverterService(UserService userService) {
         this.userService = Objects.requireNonNull(userService, "UserService must be initialised");
+        this.authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthorityPrefix("ROLE_");
+        authoritiesConverter.setAuthoritiesClaimName("permissions");
     }
-
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(this::convertAuthorities);
         return jwtAuthenticationConverter;
     }
-
     private Collection<GrantedAuthority> convertAuthorities(Jwt jwt) {
-        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
-        converter.setAuthorityPrefix("ROLE_");
-        converter.setAuthoritiesClaimName("permissions");
-        Collection<GrantedAuthority> authorities = converter.convert(jwt);
+        Collection<GrantedAuthority> authorities = authoritiesConverter.convert(jwt);
 
-        String email = jwt.getClaim("https://gift-certificates-system-api/email");
+        String email = jwt.getClaim(AUTH0_EMAIL_CLAIM);
         addRoleToAuthoritiesByEmail(authorities,email);
+//        addRoleAndUserIdToAuthoritiesByEmail(authorities,email);
         return authorities;
     }
-
     private void addRoleToAuthoritiesByEmail(Collection<GrantedAuthority> authorities, String email){
         if (email != null) {
             Optional<User> optionalUser = userService.findByEmail(email);
             if (optionalUser.isPresent()) {
                 User user = optionalUser.get();
                 authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName().toUpperCase()));
+            }
+        }
+    }
+    private void addRoleAndUserIdToAuthoritiesByEmail(Collection<GrantedAuthority> authorities, String email){
+        if (email != null) {
+            Optional<User> optionalUser = userService.findByEmail(email);
+            if (optionalUser.isPresent()) {
+                User user = optionalUser.get();
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName().toUpperCase()));
+                authorities.add(new SimpleGrantedAuthority("USER_ID_" + user.getId()));
             }
         }
     }
