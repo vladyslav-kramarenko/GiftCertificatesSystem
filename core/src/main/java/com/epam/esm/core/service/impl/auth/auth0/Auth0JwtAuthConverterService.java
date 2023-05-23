b75ffd011c3951ecd9a4d7c87f78a27b2aaf3235
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-05-23 12:19:33 -0700

    - extended auth0 Jwt Converter from my base Converter

--- core/src/main/java/com/epam/esm/core/service/impl/auth/auth0/Auth0JwtAuthConverterService.java
+++ core/src/main/java/com/epam/esm/core/service/impl/auth/auth0/Auth0JwtAuthConverterService.java
@@ -1,63 +1,35 @@
 package com.epam.esm.core.service.impl.auth.auth0;
 
-import com.epam.esm.core.entity.User;
 import com.epam.esm.core.service.UserService;
+import com.epam.esm.core.service.impl.auth.BaseJwtAuthConverterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
-import static com.epam.esm.core.util.CoreConstants.AUTH0_EMAIL_CLAIM;
+import static com.epam.esm.core.util.CoreConstants.*;
 
 @Service
-public class Auth0JwtAuthConverterService {
-
-    private final UserService userService;
+public class Auth0JwtAuthConverterService extends BaseJwtAuthConverterService {
     private final JwtGrantedAuthoritiesConverter authoritiesConverter;
+
     @Autowired
     public Auth0JwtAuthConverterService(UserService userService) {
-        this.userService = Objects.requireNonNull(userService, "UserService must be initialised");
+        super(userService);
         this.authoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        authoritiesConverter.setAuthorityPrefix("ROLE_");
+        authoritiesConverter.setAuthorityPrefix(ROLE_AUTHORITY_PREFIX);
         authoritiesConverter.setAuthoritiesClaimName("permissions");
     }
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(this::convertAuthorities);
-        return jwtAuthenticationConverter;
-    }
-    private Collection<GrantedAuthority> convertAuthorities(Jwt jwt) {
+
+    public Collection<GrantedAuthority> convertAuthorities(Jwt jwt) {
         Collection<GrantedAuthority> authorities = authoritiesConverter.convert(jwt);
+        addRolesFromClaim(authorities, jwt, AUTH0_ROLE_CLAIM);
 
         String email = jwt.getClaim(AUTH0_EMAIL_CLAIM);
-        addRoleToAuthoritiesByEmail(authorities,email);
-//        addRoleAndUserIdToAuthoritiesByEmail(authorities,email);
+        addRoleAndUserIdToAuthoritiesByEmail(authorities, email);
         return authorities;
-    }
-    private void addRoleToAuthoritiesByEmail(Collection<GrantedAuthority> authorities, String email){
-        if (email != null) {
-            Optional<User> optionalUser = userService.findByEmail(email);
-            if (optionalUser.isPresent()) {
-                User user = optionalUser.get();
-                authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName().toUpperCase()));
-            }
-        }
-    }
-    private void addRoleAndUserIdToAuthoritiesByEmail(Collection<GrantedAuthority> authorities, String email){
-        if (email != null) {
-            Optional<User> optionalUser = userService.findByEmail(email);
-            if (optionalUser.isPresent()) {
-                User user = optionalUser.get();
-                authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName().toUpperCase()));
-                authorities.add(new SimpleGrantedAuthority("USER_ID_" + user.getId()));
-            }
-        }
     }
 }
