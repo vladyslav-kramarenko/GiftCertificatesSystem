da8898b8c37062fbbc709e3104254e543d31a1ce
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-05-23 12:17:25 -0700

    - added more constants

--- core/src/main/java/com/epam/esm/core/util/CoreConstants.java
+++ core/src/main/java/com/epam/esm/core/util/CoreConstants.java
@@ -19,7 +19,12 @@ public final class CoreConstants {
     public static final String ROLE_MANAGER = "MANAGER";
     public static final String ROLE_USER = "USER";
     public static final String ROLE_ADMIN = "ADMIN";
-    public static final String GIFT_CERTIFICATE_SERVICE_TOKEN_ISSUER = "gift-certificate-service";
+    public static final String GIFT_CERTIFICATE_SERVICE_TOKEN_ISSUER = "https://gift-certificate-service.com";
     public static final String AUTH0_TOKEN_ISSUER = "https://dev-kramarenko.eu.auth0.com/";
     public static final String AUTH0_EMAIL_CLAIM = "https://gift-certificates-system-api/email";
+    public static final String AUTH0_ROLE_CLAIM = "https://gift-certificates-system-api/roles";
+    public static final String GIFT_CERTIFICATE_SERVICE_USER_ID_CLAIM = "user_id";
+    public static final String GIFT_CERTIFICATE_SERVICE_ROLES_CLAIM = "roles";
+    public static final String USER_ID_AUTHORITY_PREFIX = "USER_ID_";
+    public static final String ROLE_AUTHORITY_PREFIX = "ROLE_";
 }
