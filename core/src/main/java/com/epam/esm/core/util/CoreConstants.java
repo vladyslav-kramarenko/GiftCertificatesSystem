c04bb7fabc1cbb012cb86c7d4e1d689381e48972
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-06-29 20:02:22 -0700

    added allowed sort field

--- core/src/main/java/com/epam/esm/core/util/CoreConstants.java
+++ core/src/main/java/com/epam/esm/core/util/CoreConstants.java
@@ -11,7 +11,7 @@ public final class CoreConstants {
     public static final int ONE_HOUR = 60 * 60 * 1000;
 
     public static final int ONE_DAY = 24 * 60 * 60 * 1000;
-    public static final String[] ALLOWED_GIFT_CERTIFICATE_SORT_FIELDS = {"id", "name", "create_date", "update_date"};
+    public static final String[] ALLOWED_GIFT_CERTIFICATE_SORT_FIELDS = {"id", "name", "create_date", "update_date","price"};
     public static final String[] ALLOWED_TAG_SORT_FIELDS = {"name", "id"};
     public static final String[] ALLOWED_USER_SORT_FIELDS = {"first_name", "last_name", "id"};
     public static final String[] ALLOWED_ORDER_SORT_FIELDS = {"sum", "id"};
