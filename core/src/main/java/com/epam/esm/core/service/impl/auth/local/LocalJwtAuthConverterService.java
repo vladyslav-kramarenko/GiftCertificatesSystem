27bf348e48fe3c93f71896de0f13148644458b60
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-07-07 19:39:49 -0700

    added logger

--- core/src/main/java/com/epam/esm/core/service/impl/auth/local/LocalJwtAuthConverterService.java
+++ core/src/main/java/com/epam/esm/core/service/impl/auth/local/LocalJwtAuthConverterService.java
@@ -2,6 +2,8 @@ package com.epam.esm.core.service.impl.auth.local;
 
 import com.epam.esm.core.service.UserService;
 import com.epam.esm.core.service.impl.auth.BaseJwtAuthConverterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import static com.epam.esm.core.util.CoreConstants.*;
 
 @Service
 public class LocalJwtAuthConverterService extends BaseJwtAuthConverterService {
+    private static final Logger logger = LoggerFactory.getLogger(LocalJwtAuthConverterService.class);
     public LocalJwtAuthConverterService(UserService userService) {
         super(userService);
     }
@@ -20,6 +23,7 @@ public class LocalJwtAuthConverterService extends BaseJwtAuthConverterService {
         Collection<GrantedAuthority> authorities = new ArrayList<>();
         Long userId = jwt.getClaim(GIFT_CERTIFICATE_SERVICE_USER_ID_CLAIM);
         if (userId == null) {
+            logger.info("user_id claim is null => searching user id by email");
             String email = jwt.getSubject();
             addRoleAndUserIdToAuthoritiesByEmail(authorities, email);
         }
