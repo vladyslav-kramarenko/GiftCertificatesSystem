6b67057d9e055e55255538c0fe244cfc9e5211a4
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-07-07 19:33:42 -0700

    added configuration for new pages

--- api/src/main/java/com/epam/esm/api/config/BaseSecurityConfig.java
+++ api/src/main/java/com/epam/esm/api/config/BaseSecurityConfig.java
@@ -9,16 +9,23 @@ public abstract class BaseSecurityConfig {
     protected void configureCommon(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors().and()
                 .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
                 .requestMatchers(new AntPathRequestMatcher("/auth0/**")).permitAll()
                 .requestMatchers(HttpMethod.GET, "/certificates/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/certificates/search/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/tags/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/tags/popular").permitAll()
 
+                .requestMatchers(HttpMethod.POST, "/orders").authenticated()
                 .requestMatchers(HttpMethod.GET, "/auth/me").authenticated()
+                .requestMatchers(HttpMethod.GET, "/validate-token").authenticated()
                 .requestMatchers(new AntPathRequestMatcher("/users/{id}/**", HttpMethod.GET.name())).authenticated()
 
+                .requestMatchers(HttpMethod.PUT, "/certificates/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/certificates/**").hasRole("ADMIN")
                 .requestMatchers(new AntPathRequestMatcher("/**")).hasRole("ADMIN")
 
                 .requestMatchers(new AntPathRequestMatcher("/generate/**")).permitAll();
