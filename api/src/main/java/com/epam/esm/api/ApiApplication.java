9acf1e628fee3a75863bd51cc149847df2fb990f
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-07-07 19:31:55 -0700

    added cors headers

--- api/src/main/java/com/epam/esm/api/ApiApplication.java
+++ api/src/main/java/com/epam/esm/api/ApiApplication.java
@@ -79,7 +79,10 @@ public class ApiApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080", "http://localhost:4200");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080", "http://localhost:4200")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
             }
         };
     }
