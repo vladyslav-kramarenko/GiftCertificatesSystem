5f1ac6f19944f9fbd4e80eefd0e6a6d3a0fe6a2d
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-07-12 18:54:31 -0700

    added more repository methods

--- core/src/main/java/com/epam/esm/core/repository/RefreshTokenRepository.java
+++ core/src/main/java/com/epam/esm/core/repository/RefreshTokenRepository.java
@@ -4,12 +4,17 @@ import com.epam.esm.core.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    Optional<RefreshToken> findByUserId(Long userId);
     Optional<RefreshToken> findByToken(String token);
+
+    List<RefreshToken> findAll();
+
+    List<RefreshToken> findByUserId(Long userId);
+
     void deleteByToken(String token);
 }
