0748010ea29619cd2d79944332a2f23575826215
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-07-18 17:06:27 -0700

    added error handler for getImage

--- api/src/main/java/com/epam/esm/api/controller/ImageController.java
+++ api/src/main/java/com/epam/esm/api/controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.epam.esm.api.controller;
 
+import com.epam.esm.api.ErrorResponse;
 import com.epam.esm.core.exception.ServiceException;
 import com.epam.esm.core.service.ImgService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,12 +9,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Objects;
+
+import static com.epam.esm.api.util.Constants.ERROR_NO_CONTENT;
+
 @RestController
 @RequestMapping("/image")
 @Validated
@@ -32,12 +37,16 @@ public class ImageController {
     }
 
     @GetMapping("/{path}")
-    public ResponseEntity<Resource> getImage(@PathVariable("path") @NotNull String path, HttpServletRequest request) throws ServiceException {
-        logger.debug("request:");
-        logger.debug(String.valueOf(request));
-        Resource file = imgService.loadImageAsResource(path);
-
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    public ResponseEntity<?> getImage(@PathVariable("path") @NotNull String path, HttpServletRequest request){
+        logger.debug("request: " + request);
+        try {
+            Resource file = imgService.loadImageAsResource(path);
+            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                    "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .body(new ErrorResponse("Requested resource not found", ERROR_NO_CONTENT));
+        }
     }
 }
